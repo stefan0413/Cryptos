1dc955719d82176bee569915a95f8938f034e05f
Stefan Aleksandrov <stefan.aleksandrov@trading212.com>
2022-12-15 21:42:19 +0200

    initial project

---
+++ Customers/src/test/java/com/example/Customers/CustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
