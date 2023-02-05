aeb1ac266ac19cad94b966a02da3361172a29ad4
Stefan Aleksandrov <stefan.aleksandrov@trading212.com>
2023-02-05 21:46:05 +0200

    introduction of payments service

---
+++ Payments/src/test/java/com/cryptos/payments/PaymentsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cryptos.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+}
