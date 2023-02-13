78c05f718f682b012a1fb4e8d7dd388e886b6c04
Stefan Aleksandrov <stefan.aleksandrov@trading212.com>
2023-02-13 10:50:25 +0200

    add payment methods to customer

--- Payment/src/main/java/com/example/payment/service/PaymentCustomerService.java
+++ Payment/src/main/java/com/example/payment/service/PaymentCustomerService.java
@@ -3,17 +3,17 @@ package com.example.payment.service;
 import com.example.payment.model.CustomerDataResponse;
 import com.example.payment.rest.CustomerRestService;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Card;
 import com.stripe.model.Customer;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class PaymentCustomerService
 {
-
-	private static final String STRIPE_CUSTOMER_OBJECT = "customer";
 
 	private final CustomerRestService customerRestService;
 
@@ -31,8 +31,22 @@ public class PaymentCustomerService
 		Map<String, Object> customerParameters = new HashMap<>();
 		customerParameters.put("id", customerId);
 		customerParameters.put("email", customerData.email());
-		customerParameters.put("source", "tok_visa");
 
 		return Customer.create(customerParameters).toJson();
+	}
+
+	public String addPaymentMethodToCustomer(long customerId, String methodToken) throws StripeException
+	{
+
+		List<String> expandList = List.of("sources");
+		Map<String, Object> retrieveParams = Map.of("expand", expandList);
+
+		Customer customer = Customer.retrieve(String.valueOf(customerId), retrieveParams, null);
+
+		Map<String, Object> params = Map.of("source", methodToken);
+
+		Card card = (Card) customer.getSources().create(params);
+
+		return card.toJson();
 	}
 }
