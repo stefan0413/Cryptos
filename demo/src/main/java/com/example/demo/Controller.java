ade58d665cfd8e40a98c2b81f600dedfc9697e6c
stefan0413 <48565659+stefan0413@users.noreply.github.com>
2023-02-26 18:10:15 +0300

    test

---
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Controller {
+
+    @GetMapping
+    public String greeting()
+    {
+        return "hello";
+    }
+}
