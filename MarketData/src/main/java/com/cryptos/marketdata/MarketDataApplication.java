0d95ee43f21d26d85bfa56457893dad72cfe539a
Stefan Aleksandrov <stefan.aleksandrov@trading212.com>
2023-03-06 01:28:36 +0200

    introduce market data

---
+++ MarketData/src/main/java/com/cryptos/marketdata/MarketDataApplication.java
@@ -0,0 +1,14 @@
+package com.cryptos.marketdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDataApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MarketDataApplication.class, args);
+	}
+}
---
+++ MarketData/src/test/java/com/cryptos/marketdata/MarketDataApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cryptos.marketdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+}
